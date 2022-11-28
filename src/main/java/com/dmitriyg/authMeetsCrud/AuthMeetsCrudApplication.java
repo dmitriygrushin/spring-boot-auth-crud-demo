b9d72acaca02afbfa40de839e9b8f3cad639e8c7
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2022-11-28 17:01:09 -0500

    init commit

---
+++ src/main/java/com/dmitriyg/authMeetsCrud/AuthMeetsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.dmitriyg.authMeetsCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMeetsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMeetsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmitriyg/authMeetsCrud/AuthMeetsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitriyg.authMeetsCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMeetsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
